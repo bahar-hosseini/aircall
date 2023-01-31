15f6abd76d8e5846efa5953e08e02b64ffad7264
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-01-31 09:27:26 -0500

    remove dependency array of the useEffect in provider

--- src/components/CallListItem.jsx
+++ src/components/CallListItem.jsx
@@ -1,10 +1,36 @@
 //External Modules
 import React from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 //Internal Modules
 import '../css/CallListItem.css';
+
 const CallListItem = ({ id, from, to, isArchived }) => {
+  const navigate = useNavigate();
+
+  //Function to handle the button that sends patch request to unarchive calls
+  const handleUnarchive = () => {
+    const data = { is_archived: false };
+    axios
+      .patch(
+        `https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/activities/${id}`,
+        data,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        // navigate('/');
+      })
+      .catch((error) => {
+        console.log(`The Error is: ${error}`);
+      });
+  };
   return (
     <div className='call-container'>
       <Link to={`/${id}`}>
@@ -12,6 +38,7 @@ const CallListItem = ({ id, from, to, isArchived }) => {
         <h3>to: {to}</h3>
         <h3>is archived: {String(isArchived)}</h3>
       </Link>
+      {isArchived && <button onClick={handleUnarchive}>unarchive</button>}
     </div>
   );
 };
