f673fd60d78b745d26127aac3b8434d825646d52
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-01-30 20:58:19 -0500

    set up navbar and react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router-dom';
 
 import Header from './Header.jsx';
 import HomeScreen from './screens/HomeScreen.jsx';
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,13 +1,20 @@
 //External Modules
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 //Internal Modules
 import CallList from '../components/CallList.jsx';
+import Navbar from '../components/Navbar.jsx';
 
 const HomeScreen = () => {
   return (
     <div>
-      <CallList />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<CallList />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
