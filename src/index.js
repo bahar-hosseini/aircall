881d356af90e6ba55cfc763428d6aff798d73bf2
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-01-31 20:49:51 -0500

    comment code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './css/body.css';
 import './css/app.css';
 import './css/header.css';
-import './css/callList.css';
-import './css/callListItem.css';
+// import './css/callList.css';
+// import './css/callListItem.css';
 import './css/footer.css';
 import './css/callDetails.css';
 import './css/navbar.css';
