98d81e8b152103147299d58953fcae78454c0292
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-01-31 20:52:48 -0500

    uncomment css address

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/body.css';
 import './css/app.css';
 import './css/header.css';
-// import './css/callList.css';
+import './css/callList.css';
 // import './css/callListItem.css';
 import './css/footer.css';
 import './css/callDetails.css';
