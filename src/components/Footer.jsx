25cd45b44c3b7f7e4ff905181c2ff879e32476bd
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-01-31 19:36:46 -0500

    set up footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+//External Modules
+import React from 'react';
+
+//Internal Modules
+import '../css/footer.css';
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+export default Footer;
